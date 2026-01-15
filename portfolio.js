a95d67b48549ed19826d36455f77216f93ec9c1a
meram-amr <meramamr18@gmail.com>
2026-01-15 12:00:29 +0200

    portfolio.js

--- portfolio.js
+++ portfolio.js
@@ -52,6 +52,8 @@ const observer = new IntersectionObserver((entries) => {
     entries.forEach(entry => {
         if (entry.isIntersecting) {
             entry.target.classList.add("animate-in");
+        } else {
+            entry.target.classList.remove("animate-in");
         }
     });
 }, observerOptions);
